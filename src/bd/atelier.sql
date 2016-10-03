e6b25283142199dc43edb8bd971ffd8fad9a1e41
massaber <amine.sisaber.90@gmail.com>
2016-10-04 00:34:57 +0200

    Add files via upload

---
+++ src/bd/atelier.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 03 Octobre 2016 à 21:10
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `bd_site`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `atelier`
+--
+
+CREATE TABLE `atelier` (
+  `id_atelier` int(255) NOT NULL,
+  `titre_atelier` text NOT NULL,
+  `theme` text NOT NULL,
+  `type` text NOT NULL,
+  `langue` text NOT NULL,
+  `Niveau` text NOT NULL,
+  `capacité_accueil` int(255) NOT NULL,
+  `Adresse` text NOT NULL,
+  `durée` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `atelier`
+--
+ALTER TABLE `atelier`
+  ADD PRIMARY KEY (`id_atelier`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `atelier`
+--
+ALTER TABLE `atelier`
+  MODIFY `id_atelier` int(255) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
